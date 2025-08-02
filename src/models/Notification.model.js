f828a09853e2c77852226a1ca21dea31f3bc98e9
Mustafa Fathi <mustafa.fathi7@gmail.com>
2025-08-02 19:14:28 +0300

    test3

--- src/models/Notification.model.js
+++ src/models/Notification.model.js
@@ -1,18 +1,15 @@
-// models/Notification.model.js
 const mongoose = require("mongoose");
 
 const notificationSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     message: { type: String, required: true },
-    type: {
-      type: String,
-      enum: ["info", "warning", "success"],
-      default: "info",
-    },
     read: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Notification", notificationSchema);
+// ✅ الحل: استخدم existing model لو موجود
+module.exports =
+  mongoose.models.Notification ||
+  mongoose.model("Notification", notificationSchema);
